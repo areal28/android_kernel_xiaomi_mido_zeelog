2bb4e87473d4ef8d4b04b499eafd1af7ac1a61d7
areal28 <areal28@gmail.com>
2023-10-18 23:53:01 +0700

    rev

--- crypto/tcrypt.c
+++ crypto/tcrypt.c
@@ -73,8 +73,7 @@ static char *check[] = {
 	"cast6", "arc4", "michael_mic", "deflate", "crc32c", "tea", "xtea",
 	"khazad", "wp512", "wp384", "wp256", "tnepres", "xeta",  "fcrypt",
 	"camellia", "seed", "salsa20", "rmd128", "rmd160", "rmd256", "rmd320",
-	"lzo", "lzo-rle", "cts", "zlib", "sha3-224", "sha3-256", "sha3-384",
-	"sha3-512",
+	"lzo", "cts", "zlib", "sha3-224", "sha3-256", "sha3-384", "sha3-512",
 	NULL
 };
 
@@ -115,7 +114,7 @@ static int test_aead_jiffies(struct aead_request *req, int enc,
 	int bcount;
 	int ret;
 
-	for (start = jiffies, end = start + secs * HZ, bcount = 0;
+	for (start = jiffies, end = start + secs * msecs_to_jiffies(1000), bcount = 0;
 	     time_before(jiffies, end); bcount++) {
 		if (enc)
 			ret = do_one_aead_op(req, crypto_aead_encrypt(req));
@@ -542,7 +541,7 @@ static int test_ahash_jiffies_digest(struct ahash_request *req, int blen,
 	int bcount;
 	int ret;
 
-	for (start = jiffies, end = start + secs * HZ, bcount = 0;
+	for (start = jiffies, end = start + secs * msecs_to_jiffies(1000), bcount = 0;
 	     time_before(jiffies, end); bcount++) {
 		ret = do_one_ahash_op(req, crypto_ahash_digest(req));
 		if (ret)
@@ -565,7 +564,7 @@ static int test_ahash_jiffies(struct ahash_request *req, int blen,
 	if (plen == blen)
 		return test_ahash_jiffies_digest(req, blen, out, secs);
 
-	for (start = jiffies, end = start + secs * HZ, bcount = 0;
+	for (start = jiffies, end = start + secs * msecs_to_jiffies(1000), bcount = 0;
 	     time_before(jiffies, end); bcount++) {
 		ret = do_one_ahash_op(req, crypto_ahash_init(req));
 		if (ret)
@@ -793,7 +792,7 @@ static int test_acipher_jiffies(struct skcipher_request *req, int enc,
 	int bcount;
 	int ret;
 
-	for (start = jiffies, end = start + secs * HZ, bcount = 0;
+	for (start = jiffies, end = start + secs * msecs_to_jiffies(1000), bcount = 0;
 	     time_before(jiffies, end); bcount++) {
 		if (enc)
 			ret = do_one_acipher_op(req,
